38e69529b0c29909aeabf78847e22a1530ef87f3
김한빈 <gksqls0507@gmail.com>
2024-06-29 01:03:13 +0900

    🖼️ Add-assets:  SVG 추가 및 수정

---
+++ portfolio/public/svgs/PageDirectionSVG.tsx
@@ -0,0 +1,47 @@
+export const ScrollDownHintSVG = ()=> (
+    // <svg width="86" height="64" viewBox="0 0 86 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //     <path d="M2 19.2488L42.625 44.0148L83.25 19.2488" stroke="url(#paint0_linear_235_3680)" stroke-opacity="0.8" stroke-width="3" stroke-linecap="round"/>
+    //     <path d="M2 1L42.625 25.766L83.25 1" stroke="url(#paint1_linear_235_3680)" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round"/>
+    //     <path d="M2 36.6284L42.625 61.3945L83.25 36.6284" stroke="url(#paint2_linear_235_3680)" stroke-width="4" stroke-linecap="round"/>
+    //     <defs>
+    //     <linearGradient id="paint0_linear_235_3680" x1="83.25" y1="31.6318" x2="2" y2="31.6318" gradientUnits="userSpaceOnUse">
+    //     <stop stop-color="#875EC5"/>
+    //     <stop offset="1" stop-color="#42C48E"/>
+    //     </linearGradient>
+    //     <linearGradient id="paint1_linear_235_3680" x1="83.25" y1="13.383" x2="2" y2="13.383" gradientUnits="userSpaceOnUse">
+    //     <stop stop-color="#875EC5"/>
+    //     <stop offset="1" stop-color="#42C48E"/>
+    //     </linearGradient>
+    //     <linearGradient id="paint2_linear_235_3680" x1="83.25" y1="49.0114" x2="2" y2="49.0114" gradientUnits="userSpaceOnUse">
+    //     <stop stop-color="#875EC5"/>
+    //     <stop offset="1" stop-color="#42C48E"/>
+    //     </linearGradient>
+    //     </defs>
+    // </svg>
+<svg width="86" height="83" viewBox="0 0 86 83" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2 38.2485L42.625 63.0146L83.25 38.2485" stroke="url(#paint0_linear_326_2453)" stroke-opacity="0.8" stroke-width="3" stroke-linecap="round"/>
+<path d="M2 20L42.625 44.766L83.25 20" stroke="url(#paint1_linear_326_2453)" stroke-opacity="0.8" stroke-width="3" stroke-linecap="round"/>
+<path d="M2 20L42.625 44.766L83.25 20" stroke="url(#paint2_linear_326_2453)" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round"/>
+<path d="M2 1L42.625 25.766L83.25 1" stroke="url(#paint3_linear_326_2453)" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round"/>
+<path d="M2 55.6284L42.625 80.3945L83.25 55.6284" stroke="url(#paint4_linear_326_2453)" stroke-width="4" stroke-linecap="round"/>
+<defs>
+<linearGradient id="paint0_linear_326_2453" x1="83.25" y1="50.6316" x2="2" y2="50.6316" gradientUnits="userSpaceOnUse">
+<stop stop-color="#875EC5"/>
+<stop offset="1" stop-color="#42C48E"/>
+</linearGradient>
+<linearGradient id="paint1_linear_326_2453" x1="83.25" y1="32.383" x2="2" y2="32.383" gradientUnits="userSpaceOnUse">
+<stop stop-color="#875EC5"/>
+<stop offset="1" stop-color="#42C48E"/>
+</linearGradient>
+<linearGradient id="paint2_linear_326_2453" x1="83.25" y1="32.383" x2="2" y2="32.383" gradientUnits="userSpaceOnUse">
+<stop stop-color="#875EC5"/>
+<stop offset="1" stop-color="#42C48E"/>
+</linearGradient>
+<linearGradient id="paint4_linear_326_2453" x1="83.25" y1="68.0114" x2="2" y2="68.0114" gradientUnits="userSpaceOnUse">
+<stop stop-color="#875EC5"/>
+<stop offset="1" stop-color="#42C48E"/>
+</linearGradient>
+</defs>
+</svg>
+
+)
