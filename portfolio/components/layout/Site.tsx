cc2445baab08f7aa34068b0d40f89c71aad3c87e
김한빈 <gksqls0507@gmail.com>
2024-06-28 19:31:19 +0900

    ✨ Feat:  퀵토글 추가, 슬라이드 섹션 이동 추가

--- portfolio/components/layout/Site.tsx
+++ portfolio/components/layout/Site.tsx
@@ -2,45 +2,41 @@
  * 페이지 기본 레이아웃 (헤더,컨텐츠,푸터)
  * 
  * [TODO]
- * 1. 푸터 제작
- * 2. 스크롤 중간에 나오는 헤더는 opacity 0.4, 마우스오버시 opacity 1
- * 3. 헤더 숨김 상태일 때 헤더 아이콘 노출 시킬지 고민
- * 4. 헤더 메뉴별 마우스 오버시 효과
- * 5. 마우스 꾸밈 이벤트
- * 6. 헤더가 스크롤이 맨 처음인 상태에서 있을 때는 컨텐츠 top:100px
+ * - 푸터 제작
+ * - 스크롤 중간에 나오는 헤더는 opacity 0.4, 마우스오버시 opacity 1
+ * - 헤더 숨김 상태일 때 헤더 아이콘 노출 시킬지 고민
+ * - 헤더 메뉴별 마우스 오버시 효과
+ * - 마우스 꾸밈 이벤트
+ * - 헤더가 스크롤이 맨 처음인 상태에서 있을 때는 컨텐츠 top:100px
  * 
- * 7. 스크롤을 아래로 내려갔다가 빠르게 올라올 때, 스크린탑이 될 때 바로 컨텐츠의 top값이 조정되어야하는데
+ * - 스크롤을 아래로 내려갔다가 빠르게 올라올 때, 스크린탑이 될 때 바로 컨텐츠의 top값이 조정되어야하는데
  *    잠깐 멈칫 했다가 동작하는게 거슬림
- * 8. 스크롤 네비게이션
+ * - 스크롤 섹션 이동 시, 스크롤을 세게 당기면 화면이 충돌하듯 퉁퉁 섹션이 튕기는 무빙이 생김
+ * 
+ * - 스크롤 네비게이션(완료)
  */
 
 import { media } from '@/styles/mediaQuery';
-import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { useEffect, useRef, useState } from 'react';
 import { throttle } from 'lodash';
 import Header from '@/components/layout/Header';
-import { Element } from 'react-scroll';
-import Main from '@/components/section/01-Main';
-import Intro from '@/components/section/02-Intro';
-import Projects from '@/components/section/03-Projects';
-import Skills from '@/components/section/04-Skills';
-import Links from '@/components/section/05-Links';
+import { Element, Events, scroller } from 'react-scroll';
+import Home from '@/components/section/01-Home';
+import About from '@/components/section/02-About';
+import Skills from '@/components/section/03-Skills';
+import Projects from '@/components/section/04-Projects';
 import Contact from '@/components/section/06-Contact';
-
-interface HomeLayout {
-    children: any,
-    backgroundColor: string,
-    headerBackgroundColor: string,
-    color: string,
-}
+import QuickAccessToggle from '@/components/section_modules/QuickAccessToggle';
 
 const SiteLayout = () => {
     
-    /** 화면 스크롤(Y축) 감지 */
+    
     const [preScrollY, setPreScrollY] = useState<number>(0);
     const [isShowHeader, setIsShowHeader] = useState<boolean>(true);
     const [isScreenTop, setIsScreenTop] = useState<boolean>(true);
-
+    
+    /** 화면 스크롤(Y축) 감지 */
     useEffect(() => {
         const handleScroll = throttle(() => {
             const currentScrollY = window.scrollY;
@@ -51,7 +47,7 @@ const SiteLayout = () => {
             }
             setIsScreenTop(currentScrollY === 0);
             setPreScrollY(currentScrollY);
-        }, 200); // 100ms 간격으로 스크롤 이벤트 처리
+        }, 500); // 200ms 간격으로 스크롤 이벤트 처리
 
         window.addEventListener('scroll',handleScroll);
 
@@ -60,37 +56,63 @@ const SiteLayout = () => {
         }
     },[preScrollY, isShowHeader]);
 
+    /** 스크롤 감지 섹션 이동 */
+    useEffect(() => {
+        const sections = Array.from(document.querySelectorAll("[data-section]"));
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const sectionName = entry.target.getAttribute('data-section');
+                    if (sectionName) {
+                        scroller.scrollTo(sectionName, {
+                            duration: 1000,
+                            smooth: 'easeOutQuad',
+                            onComplete: () => {
+                            }
+                        });
+                    }
+                }
+            });
+        }, {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.01 // 섹션의 50%가 보이면 감지
+        });
+
+        sections.forEach(section => observer.observe(section));
+
+        return () => {
+            sections.forEach(section => observer.unobserve(section));
+        };
+    }, []);
+
     return (
-        <Layout style={{background:'linear-gradient(132deg, #2e2e2e, #000)'??'#fff'}}>
-            <Header showHeader={isShowHeader} />
-            {/* <Content isScreenTop={isScreenTop}> */}
-            <Element name="main">
-                <Main/>
-            </Element>
-            <BackLayout>
-                <video autoPlay muted playsInline>
-                    <source src="/videos/smokeBackground.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                <Element name="intro">
-                    <Intro />
+        <Layout isShowHeader={isShowHeader}style={{background:'linear-gradient(132deg, #2e2e2e, #000)'??'#fff'}}>
+            <Header isShowHeader={isShowHeader}/>
+                <Element name="home" data-section="home">
+                    <Home/>
                 </Element>
-            </BackLayout>
-            <Element name="project">
-                <Projects/>
-            </Element>
-            <SkillLayout>
-            <Element name="skills">
-                <Skills/>
-            </Element>
-            </SkillLayout>
-            <Element name="links">
-                <Links/>
-            </Element>
-            <Element name="contact">
-                <Contact/>
-            </Element>
-            {/* </Content> */}
+                    <Element name="about" data-section="about">
+                        <About />
+                    </Element>
+                <SkillLayout>
+                    <Element name="skills" data-section="skills">
+                        <Skills/>
+                    </Element>
+                </SkillLayout>
+                <Element name="projects" data-section="projects">
+                    <Projects/>
+                </Element>
+                <Element name="contact" data-section="contact">
+                    <Contact/>
+                </Element>
+            <QuickToggle>
+                <Toggle className="QuickAccessToggle" isScreenTop={isScreenTop}>
+                    <QuickAccessToggle/>
+                </Toggle>
+                <div className="toggleBackground"/>
+            </QuickToggle>
             <Footer style={{backgroundColor:'#000'}}>
                 <div>
                     © 2024 KIMHANBEEN PORTFOLIO. All rights reserved.<br/>
@@ -106,33 +128,24 @@ const SiteLayout = () => {
 };
 export default SiteLayout;
 
-const Layout = styled.div`
+const toggleBackground = keyframes`
+    0%{
+        opacity: 0;
+    }
+    100%{
+        opacity: 1;
+    }
+`;
+
+const Layout = styled.div<{isShowHeader:boolean}>`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: start;
+    position: relative;
     width: 100%;
     min-height: calc(100vh + 100px);
     height: 100%;
-`;
-
-const BackLayout = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    width: 100%;
-    height: 100%;
-
-    video {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        pointer-events: none;
-        object-fit: cover;
-    }
 `;
 
 const Footer = styled.div`
@@ -154,6 +167,34 @@ const SkillLayout = styled.div`
     min-height: 100vh;
     height: 100%;
 
-    background: url('images/smoke14.png');
+    background: url('images/smoke09.png');
     background-size: cover;
+`;
+
+
+const QuickToggle = styled.div`
+
+    & .toggleBackground{
+        display: none;
+    };
+
+    &:hover .toggleBackground{
+        display: flex;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: linear-gradient(to right, #00000015 20%, #0000009b 80%);
+        pointer-events: none;
+        animation: ${toggleBackground} 0.5s forwards;
+    }
+`;
+
+const Toggle = styled.div<{isScreenTop:boolean}>`
+    position: fixed;
+    bottom: ${({isScreenTop})=>(isScreenTop?'20px':'50px')};
+    right: 2.5%;
+    transition: bottom 1.5s;
+    z-index: 1;
 `;
